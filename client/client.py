fc2e057a81db3b57c5f9a87597b05a184f23ab55
Andrea Oquendo <oquendo.3455@gmail.com>
2024-11-12 22:34:02 -0300

    missing retransmission

--- client/client.py
+++ client/client.py
@@ -1,32 +1,126 @@
 import socket
+import zlib
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 REQUEST_FILE = "image.jpg"
-MESSAGE = f"GET /{REQUEST_FILE}"
-BUFFER_SIZE = 4000
+MESSAGE = f"Hello"
+BUFFER_SIZE = 8000
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
+def retransmit_message(filename, chunk_number):
+    
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+if __name__ == "__main__":
 
-with open("recebido_" + REQUEST_FILE, "wb") as f:
-    while True:
-        data, _ = sock.recvfrom(BUFFER_SIZE)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = -1
+    while addr == -1:
+        addr = input("Insert server ip:port: ")
+        try:
+            ip, port = addr.split(":")
+            sock.sendto(MESSAGE.encode(), (ip, int(port)))
+            print(f"Message sent to {UDP_IP}:{UDP_PORT}")
+            
+            sock.settimeout(2)
+            try:
+                response, server = sock.recvfrom(BUFFER_SIZE)
+                print(f"Server responding normally")
+            except socket.timeout:
+                print("No response received; the server may not be reachable or is not responding.")
+            
+        except Exception as error:
+            print(f"Error connecting to server: {error}")
+            addr = -1
+
+    while True: 
+        message = input("Insert command [GET /<file> -m <recieved file name>| RETRANSMIT <chunk number> \<file> ]:\n")
+        command, file_name = message.split(" -m ")
+        sock.sendto(command.encode(), (UDP_IP, UDP_PORT))
+        recieved_chunks = []
+        while True:
+            data, _ = sock.recvfrom(BUFFER_SIZE)
+            
+            if data == b"END":
+                print("End of transmission.")
+                break
+            
+            if data.startswith(b"ERROR"):
+                print(data.decode())
+                break
+
+            recieved_chunks.append(data)         
         
-        # Verifica o fim da transmissão
-        if data == b"END":
-            print("Arquivo recebido com sucesso.")
-            break
-        
-        # Verifica mensagem de erro
-        if data.startswith(b"ERROR"):
-            print(data.decode())
-            break
+        print("Verifying file...")
+        drop_chunk = input(f"recieved {len(recieved_chunks)}, drop chunk? (number ex: 1,4,5/not)")
+        delete_chunks = []
+        if drop_chunk != "n":
+            delete_chunks = drop_chunk.split(",")
+            delete_chunks = [int(x.strip()) for x in delete_chunks]
+        else: 
+            print("yes?")
+        print(delete_chunks)
 
-        # Obtém o número do pedaço e os dados
-        chunk_info, file_data = data.split(b":", 1)
-        f.write(file_data)  # Salva o pedaço no arquivo
+        chunks_dict = {}
+        for chunk in recieved_chunks:
+            chunk_number, checksum, file_data = chunk.split(b":", 2)
+            received_checksum = int(checksum.decode())
+            calculated_checksum = zlib.crc32(file_data)
+            if received_checksum != calculated_checksum:
+                reply = input(f"Error: Incorrect checksum on {chunk_number.decode()}. Retransmit?")
+
+                if reply != "y":
+                        print("Aborting...")
+                
+                _, filename = command.split(" ", 1)
+                retransmit_command = f"RETRANSMIT {chunk_number} {file_name}"
+                sock.sendto(command.encode(), (UDP_IP, UDP_PORT))
+                while True:
+                    data, _ = sock.recvfrom(BUFFER_SIZE)
+                    
+                    if data == b"END":
+                        print("End of transmission.")
+                        break
+                    
+                    if data.startswith(b"ERROR"):
+                        print(data.decode())
+                        break
+
+                    recieved_chunks.append(data) 
+                  
+            chunks_dict[int(chunk_number)] = file_data
+
+        print(chunks_dict.keys())
+        for i in delete_chunks:
+            chunks_dict.pop(i)
+        print(chunks_dict.keys())
+
+        max_size = max(chunks_dict) + 1
+        for i in range(0, max_size):
+            if i not in chunks_dict.keys():
+                reply = input(f"Chunk number {i} lost. Retransmit? (y/n) ")
+                if reply != "y":
+                    print("Aborting...")
+                
+                _, filename = command.split(" ", 1)
+                retransmit_command = f"RETRANSMIT {i} {filename}"
+                sock.sendto(command.encode(), (UDP_IP, UDP_PORT))
+                while True:
+                    data, _ = sock.recvfrom(BUFFER_SIZE)
+                    
+                    if data == b"END":
+                        print("End of transmission.")
+                        break
+                    
+                    if data.startswith(b"ERROR"):
+                        print(data.decode())
+                        break
+
+                    recieved_chunks.append(data)
+                    chunks_dict[i] = data  
+
+
+        with open("recebido_" + file_name, "wb") as f:   
+            for i in sorted(chunks_dict.keys()):
+                f.write(chunks_dict[i])
+            f.close()
+                
--- server/server.py
+++ server/server.py
@@ -1,50 +1,85 @@
 import socket
 import os
+import zlib
  
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
-BUFFER_SIZE = 4000
+BUFFER_SIZE = 7800
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
 def get_file(sock, addr, filename):
     if not os.path.isfile(filename):
-        error_message = "ERROR: Arquivo não encontrado".encode()
+        error_message = "ERROR: File not found".encode()
         sock.sendto(error_message, addr)
         return
 
-    print("found file")
     with open(filename, "rb") as f:
         chunk_number = 0
         while True:
-            data = f.read(BUFFER_SIZE)  # Lê um pedaço do arquivo
+            data = f.read(BUFFER_SIZE) 
             if not data:
-                break  # Sai do loop quando não há mais dados
+                break  
+            checksum = zlib.crc32(data)
             
-            # Anexa o número do pedaço ao início dos dados
-            packet = f"{chunk_number}:".encode() + data
+            packet = f"{chunk_number}:{checksum}:".encode() + data
             sock.sendto(packet, addr)
             chunk_number += 1
 
-        # Envia uma mensagem de fim de transmissão
         sock.sendto(b"END", addr)
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
-
-    request = data.decode()
-    if request.startswith("GET"):
-        print("GET REQUEST")
-        _, filename = request.split(" ", 1)
-        filename = filename.strip("/")
-        get_file(sock, addr, filename)
-    elif request.startswith("POST"):
-        sock.sendto("post request".encode(), addr)
-        _, filename = request.split(" ", 1)
-        filename = filename.strip("/")
-        # send_file(sock, addr, filename)
-    else:
-        error_message = "ERROR: Comando inválido".encode()
+def retransmit_file(sock, addr, filename, chunk):
+    if not os.path.isfile(filename):
+        error_message = "ERROR: File not found".encode()
         sock.sendto(error_message, addr)
+        return
+
+    file_dict = {}
+    with open(filename, "rb") as f:
+        chunk_number = 0
+        while True:
+            data = f.read(BUFFER_SIZE) 
+            if not data:
+                break  
+            file_dict[chunk_number] = data
+            chunk_number += 1
+
+    try:
+        data = file_dict[chunk]
+        checksum = zlib.crc32(data)
+        packet = f"{chunk_number}:{checksum}:".encode() + data
+        sock.sendto(packet, addr)
+        sock.sendto(b"END", addr)
+    except Exception as e:
+        error_message = "ERROR: {e}"
+        sock.sendto(error_message, addr)
+        return
+
+if __name__ == "__main__":
+    port = -1
+    while port == -1: 
+        port = input("Insert server port: ")
+        try: 
+            port = int(port)
+            sock.bind((UDP_IP, port))
+            print(f"Server running on {UDP_IP}:{port}")
+        except Exception as e:
+            print(e)
+            print(f"Invalid port {port}")
+            port = -1
+
+    while True:
+        data, addr = sock.recvfrom(2048) 
+
+        request = data.decode()
+        if request.startswith("GET"):
+            _, filename = request.split(" ", 1)
+            filename = filename.strip("/")
+            get_file(sock, addr, filename)
+        elif request.startswith("RETRANSMIT"):
+            _, chunk, filename = request.split(" ", 2)
+            filename = filename.strip("/")
+            retransmit_file(sock, addr, filename, int(chunk))
+        else:
+            error_message = "ERROR: Comando inválido".encode()
+            sock.sendto(error_message, addr)
